2ecdfaac896b2b6c914848c2684e10136340326a
citaconrama <48551285+StarBotica@users.noreply.github.com>
2023-02-14 13:52:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ st.header("Calculadora")
 radio=st.number_input("Radio de la circunferencia")
 st.write("El diámetro de la circunferencia es",radio*2*math.pi)
 st.write("El área de la circunferencia es",math.pi*radio*radio)
-st.write("El volumen de la esfera es",4*math.pi*pow(radio,3)/3
+st.write("El volumen de la esfera es",4*math.pi*pow(radio,3)/3)
