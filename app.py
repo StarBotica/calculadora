bd8b50267c9eb365afe42ee2912c0f5ff4de03f0
citaconrama <48551285+StarBotica@users.noreply.github.com>
2023-02-14 13:46:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.header("Calculadora")
 
 radio=st.number_input("Radio de la circunferencia")
-st.write("El diámetro de la circunferencia es",radio*2*math.PI)
+st.write("El diámetro de la circunferencia es",radio*2*math.pi)
